94b2ccc371c811b4e0d567832ac0af8e91ce0ae1
Markus Rekkenbeil <markus@bionix-it.de>
2013-07-26 15:21:19 +0200

    Bugfix: Add closing bracket and sytel up the code
    Signed-off-by: Markus Rekkenbeil <markus@bionix-it.de>

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,9 +1,10 @@
 # This class stops the puppet daemon, because we use the cron tasks
 class puppet::service {
-service { "puppet":
-  ensure => stopped,
-  hasstatus => true,
-  hasrestart => true,
-  enable => false,
-  require => Class["puppet::cron"],
+  service { "puppet":
+    ensure        => stopped,
+    hasstatus     => true,
+    hasrestart    => true,
+    enable        => false,
+    require       => Class["puppet::cron"],
+  }
 }
