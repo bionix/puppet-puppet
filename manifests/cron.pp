a8b985a3b7bb244339acf793f1a527bbfa953d22
Markus Rekkenbeil <markus@bionix-it.de>
2013-07-26 15:42:28 +0200

    Bugfix: time variable
    Signed-off-by: Markus Rekkenbeil <markus@bionix-it.de>

--- manifests/cron.pp
+++ manifests/cron.pp
@@ -4,7 +4,7 @@ class puppet::cron (
     $puppetenvironment  = undef,
 ){
     $puppetcronminute   = fqdn_rand(30)
-    $puppetcronminute1  = ${puppetcronminute} + 30
+    $puppetcronminute1  = $puppetcronminute + 30
   cron { 'puppet':
     ensure  => present,
     command => "puppet agent --onetime --no-daemonize --environment ${puppetenvironment} --fqdn ${::fqdn} --server ${puppetserver} --logdest syslog > /dev/null 2>&1",
